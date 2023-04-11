c03d254aba4a90595ffb8e45cb3aecbc4c7d4744
byul2004 <choibyeolyi@gmail.com>
2023-04-12 01:17:52 +0900

    별이 업로드 합니당7 ~두구두구~

--- src/main/java/org/zerock/myapp/domain/MemberVO.java
+++ src/main/java/org/zerock/myapp/domain/MemberVO.java
@@ -7,34 +7,17 @@ import oracle.sql.TIMESTAMP;
 @Value
 public class MemberVO {
 
-	private String id;
-	private String password;
-	private String name;
-	private String email;
-	private String tel;
-	private Integer address1;
-	private String address2;
-	private String address3;
-	private String gender;
-	private TIMESTAMP signup_date;
-	private String birth_date;
-	private Integer adminCk;
-	
-<<<<<<< HEAD
-//	// 혁규
-//	private String memberId;
-//	private String memberPw;
-//	private String memberName;
-//	private String memberMail;
-//	private String memberHp;
-//	private Integer memberAddr1;
-//	private String memberAddr2;
-//	private String memberAddr3;
-//	private String gender;
-//	private TIMESTAMP joinDate;
-//	private String birthDate;
-//	private Integer adminCk;
-	
-=======
->>>>>>> 77d9f86eb9332fa8eaf9a29eacb12dd85bcef89f
+   private String id;
+   private String password;
+   private String name;
+   private String email;
+   private String tel;
+   private Integer address1;
+   private String address2;
+   private String address3;
+   private String gender;
+   private TIMESTAMP signup_date;
+   private String birth_date;
+   private Integer adminCk;
+   
 } // end class
