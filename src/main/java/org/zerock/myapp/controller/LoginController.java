832a3db3a8ce2ea6aa09e44a5437c3028878c807
sengna_sub <jeonseino.o@gmail.com>
2023-04-17 17:35:00 +0900

    [셍나] 로그인 컨트롤러, MemberMapper.xml 로그인 파트 수정 중

--- src/main/java/org/zerock/myapp/controller/LoginController.java
+++ src/main/java/org/zerock/myapp/controller/LoginController.java
@@ -9,14 +9,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.myapp.domain.LoginDTO;
 import org.zerock.myapp.domain.MemberVO;
 import org.zerock.myapp.service.MemberService;
 
-import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
 @Log4j2
-@AllArgsConstructor
+@NoArgsConstructor
 
 @RequestMapping("/login") 
 @Controller
@@ -24,7 +25,7 @@ public class LoginController {
 	
 	@Autowired
 	private MemberService memberService;
-	
+
 	// 단순 로그인 화면으로 진입
 	@GetMapping("")
 	public String loginPageGET() {
@@ -39,17 +40,17 @@ public class LoginController {
     @PostMapping("/main")
     public String loginPost(
     		HttpServletRequest request, 
-//    		LoginDTO loginDTO,
-    		MemberVO memberVO,
+    		LoginDTO loginDTO,
+//    		MemberVO memberVO,
     		RedirectAttributes rttr
     ) throws Exception {
 
         log.trace("memberLogin 메소드에 진입하였습니다.");
-        log.info("전달된 데이터는 {}입니다.", memberVO);
-//        log.info("전달된 데이터는 {}입니다.", loginDTO);
+//        log.info("전달된 데이터는 {}입니다.", memberVO);
+        log.info("전달된 데이터는 {}입니다.", loginDTO);
         
         HttpSession session = request.getSession();
-        MemberVO m_vo = memberService.memberLogin(loginDTO);
+        MemberVO m_vo = this.memberService.memberLogin(loginDTO);
        
         if(m_vo == null) {                                // 일치하지 않는 아이디, 비밀번호 입력 경우
             
@@ -60,9 +61,7 @@ public class LoginController {
             
         } // if
         
-        // setAttribute -> member로 담아줘야 함... 왜냐?
-        // 뒤에 사람들이 이미 담아벌임...
-        // 나도 안담았는데..? -ㅇ-!!!
+        // setAttribute -> member로 담아줘야 함
         session.setAttribute("member", m_vo);             // 일치하는 아이디, 비밀번호 경우 (로그인 성공)
         
         return "redirect:main";
