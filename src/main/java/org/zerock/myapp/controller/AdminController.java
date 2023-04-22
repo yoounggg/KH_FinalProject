d3d9be641b45294e4ad340baca5311c04db3a847
byul2004 <choibyeolyi@gmail.com>
2023-04-22 16:36:03 +0900

    [별이] 고객센터(faq, notice, 이용약관) 메인페이지와 연결

--- src/main/java/org/zerock/myapp/controller/AdminController.java
+++ src/main/java/org/zerock/myapp/controller/AdminController.java
@@ -18,7 +18,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.myapp.domain.AttachImageVO;
@@ -101,22 +100,170 @@ public class AdminController {
    public String register(ProductDTO dto, RedirectAttributes rttrs) 
          throws ControllerException {
       
-         log.trace("register({}, {}, {}) invoked. (관리자 페이지 상품 등록-POST)", dto, rttrs);
+         log.trace("register({}, {}) invoked. (관리자 페이지 상품 등록-POST)", dto, rttrs);
       
          try {
-        	        	 
             boolean success = this.service.register(dto);
             log.info("\t: success: {}", success);
 
+            // 비지니스 요청 처리용 전송파라미터 전송처리
             rttrs.addAttribute("result", (success)? "success" : "failure"); 
-         
+            // KEY = 등록처리결과
+            
             return "redirect:/admin/main"; // 실패했든 성공했든 여기로 이동
          } catch (Exception e) {
             throw new ControllerException(e);
          } // try-catch
       
    } // register-post
-  
+   
+   
+   
+   /* [별이 4/17] 첨부 파일 업로드용 컨트롤러 */
+   @PostMapping(value="/uploadAjaxAction", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+   public ResponseEntity<List<AttachImageVO>> uploadAjaxActionPOST(MultipartFile[] main_image) throws ControllerException {
+      
+      log.info("uploadAjaxActionPOST......");
+      
+      
+      /* 이미지 파일 체크 */
+      for(MultipartFile multipartFile: main_image) {
+         
+         File checkfile = new File(multipartFile.getOriginalFilename());
+         String type = null;
+         
+         try {
+            type = Files.probeContentType(checkfile.toPath());
+            log.info("MIME TYPE : "+ type);
+         } catch (IOException e) {
+            throw new ControllerException(e);
+         } // try-catch
+         
+         if(!type.startsWith("image")) {
+            
+            List<AttachImageVO> list = null;
+            return new ResponseEntity<>(list, HttpStatus.BAD_REQUEST);
+            
+         } // if
+         
+      } // for
+      
+      String uploadFolder = "C:\\upload";
+      
+      /* 날짜 폴더 경로 */
+      SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+      Date date = new Date();
+      String str = sdf.format(date);
+      String datePath = str.replace("-", File.separator);
+      
+      /* 폴더 생성 */
+      File uploadPath = new File(uploadFolder, datePath);
+      
+      if(uploadPath.exists() == false) {
+         uploadPath.mkdirs();
+      }
+      
+      /* 이미지 정보 담는 객체 */
+      List<AttachImageVO> list = new ArrayList();
+      
+      /* 향상된 for */
+      for(MultipartFile multipartFile : main_image) {
+         log.info("==================================================");
+         log.info("파일 이름 : " + multipartFile.getOriginalFilename());
+         log.info("파일 타입 : " + multipartFile.getContentType());
+         log.info("파일 크기 : " + multipartFile.getSize());
+         
+         /* 이미지 정보 객체 */
+         AttachImageVO vo = new AttachImageVO();
+         
+         /* 파일 이름 */
+         String uploadFileName = multipartFile.getOriginalFilename();
+         vo.setFileName(uploadFileName);
+         vo.setUploadPath(datePath);
+         
+         /* uuid 적용 파일 이름 - 각 파일이 저장될 때 고유한 이름을 갖도록 함*/
+         String uuid = UUID.randomUUID().toString();
+         uploadFileName = uuid + "_" + uploadFileName;
+         vo.setUuid(uuid);
+         
+         /* 파일 위치, 파일 이름을 합친 File 객체 */
+         File saveFile = new File(uploadPath, uploadFileName);
+         
+         /* 파일 저장 */
+         try {
+            multipartFile.transferTo(saveFile);
+            
+            /* 썸네일 이미지 저장 */
+            File sub_image1 = new File(uploadPath, "s_" + uploadFileName);
+            File sub_image2 = new File(uploadPath, "s_" + uploadFileName);
+            File sub_image3 = new File(uploadPath, "s_" + uploadFileName);
+            File sub_image4 = new File(uploadPath, "s_" + uploadFileName);
+            
+            File thumnailFile = new File(uploadPath, "s_" + uploadFileName); 
+            
+               Thumbnails.of(saveFile)
+               .size(160, 160)
+               .toFile(sub_image1);
+               
+               
+               Thumbnails.of(saveFile)
+               .size(160, 160)
+               .toFile(sub_image2);
+               
+               
+               Thumbnails.of(saveFile)
+               .size(160, 160)
+               .toFile(sub_image3);
+               
+               
+               Thumbnails.of(saveFile)
+               .size(160, 160)
+               .toFile(sub_image4);
+            
+         } catch(Exception e) {
+            throw new ControllerException(e);
+         } // try-catch
+         
+         list.add(vo);
+         
+      } // for
+      
+      ResponseEntity<List<AttachImageVO>> result = new ResponseEntity<List<AttachImageVO>>(list, HttpStatus.OK);
+      
+      return result;
+      
+   } //uploadAjaxActionPOST
+   
+   
+   /* 이미지 삭제 */
+   @PostMapping("/deleteFile")
+   public ResponseEntity<String> deleteFile(String fileName) {
+      
+      log.info("deleteFile....................." + fileName);
+      
+      File file = null;
+      
+      try {
+         /* 썸네일 파일 삭제 */
+         file = new File("C:\\upload\\" + URLDecoder.decode(fileName, "UTF-8"));
+         file.delete();
+         
+         /* 원본파일 삭제 */
+         String originFileName = file.getAbsolutePath().replaceFirst("s_", "");
+         log.info("originFileName : "+originFileName);
+         file = new File(originFileName);
+         file.delete();
+         
+         
+      }catch (Exception e) {
+
+         e.printStackTrace();
+         return new ResponseEntity<String>("fail", HttpStatus.NOT_IMPLEMENTED);
+      } // try-catch
+      
+      return new ResponseEntity<String>("success", HttpStatus.OK);
+      
+   } // deleteFile
    
    
 } // end class
