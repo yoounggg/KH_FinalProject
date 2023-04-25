692f8b886e6a7aecf0d71dff887a5e1917280729
Hyeokgyu Kwon <kog369nk@naver.com>
2023-04-25 13:45:46 +0900

    [혁규] 소셜 회원가입 일반 회원가입과 통합 수정2

--- src/main/java/org/zerock/myapp/service/KakaoService.java
+++ src/main/java/org/zerock/myapp/service/KakaoService.java
@@ -6,7 +6,8 @@ public interface KakaoService {
 
 	//access token 얻기
 	public String getAccessToken(String code) throws Throwable;
-		
+	
+	//user 정보 가져오기
 	public Map<String, Object> getUserInfo(String access_token) throws Throwable;
 	
 } // end interface
