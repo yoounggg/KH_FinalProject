a790776ef75ab59b40b16306a6363e06f61acbc2
yoounggg <110173325+yoounggg@users.noreply.github.com>
2023-05-03 19:08:48 +0900

    [채영] 메인, 장바구니, 개인정보수정 오류 수정(05/03)

--- src/main/webapp/resources/js/modifypw.js
+++ src/main/webapp/resources/js/modifypw.js
@@ -1,14 +1,5 @@
 //상단 nav 메뉴 고정
-//$(document).ready(function(){
-//    var menuOffset = $('.menu').offset();
-//    $(window).scroll(function(){
-//      if($(document).scrollTop() > menuOffset.top){
-//        $('.menu').addClass('fixedmenu');
-//      } else{
-//        $('.menu').removeClass('fixedmenu');
-//      }
-//    });
-//  });
+
 $(document).ready(function(){
    var menuOffset = $('.menu').offset();
    $(window).scroll(function(){
