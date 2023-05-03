9e20f625c4d773896b7d358325855c4b79853e8b
오찬석 <dhcksehf1@naver.com>
2023-05-03 09:37:29 +0900

    [찬돌] 결제api 후 db저장까지 완료

--- src/main/webapp/resources/js/order/order.js
+++ src/main/webapp/resources/js/order/order.js
@@ -1,4 +1,4 @@
-  /*  		
+    		
         IMP.init("imp11157675"); // 예: imp00000000a
 
         function requestPay() {
@@ -17,7 +17,7 @@
                 pay_method : 'card',
                 merchant_uid: merchant_uid, 
                 name : productName,
-                amount : price,
+                amount : 101,
                 buyer_email : email,
                 buyer_name : name,
                 buyer_tel : tel,
@@ -26,40 +26,19 @@
             }, function (rsp) { // callback
                 if(rsp.success) {
                     console.log(rsp);
-					var dto = {
-						productId: productId,
-						productName: productName,
-						price: price,
-						email: email,
-						name: name,
-						tel: tel,
-						addr: addr,
-						postcode: postcode,
-						merchantUid: merchant_uid,
-						uid: uid
-       			 	};
-       			 	$.ajax({
-			          url: '/order/${uid}',
-			          method: 'POST',
-			          contentType: "application/json",
-			          dataType: "json",
-			          data: JSON.stringify(dto),
-			          success: function(data) {
-			            console.log(data);
-			            window.location.href = "/order";
-			          },
-			          error: function(xhr, status, error) {
-			            console.log(xhr);
-			          }
-			        });
+					sendOrderInfo();
                 } else {
                     console.log(rsp);
                 }
             });
         }
-*/
+
 $(".order_btn").on("click", function() {
-        
+          
+         requestPay();      
+    }); 
+    
+            function sendOrderInfo() {
         // 배송 요청사항 select 태그에서 선택한 option 값 받아오기
         var deliveryRequest = $("#배송요청사항 option:selected").val();
         // 만약 "직접입력"을 선택했다면 해당 input 태그의 값을 deliveryRequest 변수에 저장하기
@@ -106,9 +85,7 @@ $(".order_btn").on("click", function() {
     
           /* 주문 양식 제출 */
           $(".order_form").submit();   
-          
-  /*        requestPay();  */     
-    }); 
+        }
 
 
 /* ============================== 배송요청사항 직접입력 =========================================== */
