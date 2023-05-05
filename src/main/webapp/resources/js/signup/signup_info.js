07d909902766067effd8543c7e46d7f363886a4c
Hyeokgyu Kwon <kog369nk@naver.com>
2023-05-05 15:11:40 +0900

    [혁규] 에러 해결 중

--- src/main/webapp/resources/js/signup/signup_info.js
+++ src/main/webapp/resources/js/signup/signup_info.js
@@ -270,7 +270,7 @@ function num_compare() {
         popup_on10();                                   // 인증되었습니다.
         clearInterval(timer);                           // 타이머 멈춤
         document.getElementById("num_form").style.display = "none";  // 인증번호 입력창 숨김
-
+        
     } else if ($("#input_num").val() != random_num) {   // 다르다면
         popup_on8();                                    // 인증번호가 틀렸습니다.
 
